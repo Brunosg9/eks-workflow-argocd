724418b6fe11d0b8d04cde2b47026c075fa2cd64
Brunosg9 <brunog.silva@live.com>
2025-07-27 11:56:31 -0300

    triggers-terraform

--- vpc/vpc.private.route.tf
+++ vpc/vpc.private.route.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "private" {
   count   = length(var.vpc_id.private_route_table_name)
   vpc_id  = aws_vpc.this.id
   route {
-    cidr_block = 0.0.0.0/0
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.this[count.index].id
   }
   tags = {
