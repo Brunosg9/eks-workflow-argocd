8e4e7d0a077b210199d7f6bd04b544868da63d39
Brunosg9 <brunog.silva@live.com>
2025-07-27 12:23:28 -0300

    terraform-trigger

--- vpc/vpc.nat.gateway.tf
+++ vpc/vpc.nat.gateway.tf
@@ -2,6 +2,6 @@ resource "aws_nat_gateway" "this" {
   count         = length(var.vpc_id.nat_gateway_name)
   allocation_id = aws_eip.this[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
-  tags = { Name = "${var.environment}-${var.vpc_id.nat_gateway_name}" }
+  tags = { Name = "${var.environment}-${var.vpc_id.nat_gateway_name[count.index]}" }
   depends_on    = [aws_internet_gateway.this]
 }
