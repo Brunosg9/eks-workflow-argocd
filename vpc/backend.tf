5bf40abe76ffd6fed07654e3ce8afc691f4e96f7
Brunosg9 <brunog.silva@live.com>
2025-07-27 20:28:39 -0300

    test-backend

---
+++ vpc/backend.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1"  # ajuste para sua região
+}
+
+# Bucket S3 - já criado manualmente no console
+data "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "github-s3-terraform-tfstate"
+}
+
+# Tabela DynamoDB para locking do Terraform state
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "prd"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "github-s3-terraform-tfstate"
+    key            = "vpc/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = aws_dynamodb_table.terraform_locks.name
+    encrypt        = true
+  }
+}
