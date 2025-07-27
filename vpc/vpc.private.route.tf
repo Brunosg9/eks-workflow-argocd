5b847cf9a61920e961704e95f4c669c56711bd81
Brunosg9 <brunog.silva@live.com>
2025-07-27 12:16:31 -0300

    terraform

--- vpc/vpc.private.route.tf
+++ vpc/vpc.private.route.tf
@@ -3,10 +3,10 @@ resource "aws_route_table" "private" {
   vpc_id  = aws_vpc.this.id
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
+    nat_gateway_id = aws_nat_gateway.this[count.index].id
   }
   tags = {
-    Name = "${var.environment}-${var.vpc_id.private_route_table_name[count.index]}"
+    Name = "${var.environment}-${var.vpc_id.private_route_table_name[0]}"
   }
 }
 
