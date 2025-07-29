3df9f7863c7c8ab245e1d8e9248927f34869b6d2
Brunosg9 <brunog.silva@live.com>
2025-07-28 22:08:44 -0300

    eks-cluster-karpenter

--- eks/eks.cluster.node-group.tf
+++ eks/eks.cluster.node-group.tf
@@ -2,7 +2,7 @@ resource "aws_eks_node_group" "this" {
     cluster_name    = "${aws_eks_cluster.this.name}"
     node_group_name = "${var.eks_cluster.name}-node-group"
     node_role_arn   = aws_iam_role.eks_node_group_role.arn
-    subnets_ids     = data.terraform_remote_state.eks.outputs.private_subnet_ids
+    subnet_ids     = data.terraform_remote_state.eks.outputs.private_subnet_ids
     capacity_type   = var.eks_cluster.node_group.capacity_type
     instance_types  = var.eks_cluster.node_group.instance_type
 
