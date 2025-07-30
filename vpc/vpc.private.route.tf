263fd3cc6d6b168b48d310a5a2fe86bfb620f1b2
Brunosg9 <brunog.silva@live.com>
2025-07-29 21:45:41 -0300

    trigger-vpc

--- vpc/vpc.private.route.tf
+++ vpc/vpc.private.route.tf
@@ -13,5 +13,5 @@ resource "aws_route_table" "private" {
 resource "aws_route_table_association" "private" {
   count          = length(aws_subnet.private)
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
+  route_table_id = aws_route_table.private[0].id
 }
